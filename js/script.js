5e6440908362014f6b1980e3e40fc71c4745fc0a
LuckeyBairagi <lbairagi98@gmail.com>
2021-05-19 17:04:07 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+var navToggle = document.querySelector(".nav-toggle");
+var navLinks = document.querySelectorAll(".nav__link");
+navToggle.addEventListener("click" , ()=>{
+    document.body.classList.toggle("nav-open");
+})
+navLinks.forEach(link=>{
+    link.addEventListener('click' , ()=> {
+        document.body.classList.remove("nav-open");
+    })
+})
